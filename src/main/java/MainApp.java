481da56374da2a5288263d87e31b2438428ba54b
HuynhNguyenV <nguyenviethuynh1996.vih@gmail.com>
2018-06-26 14:22:54 +0700

    Initial commit

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,20 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+public class MainApp {
+    public static void main(String[] args){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        MailSender mailSender = (MailSender) context.getBean("mailSender");
+        System.out.println("Sending text...");
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("h.nguyen03101996@gmail.com");
+        message.setTo("nguyenviethuynh1996.vih@gmail.com");
+        message.setSubject("Subject");
+        message.setText("thang cong vang doi 2");
+        //sending message
+        mailSender.send(message);
+        System.out.println("Sending text done!!!");
+        context.close();
+    }
+}
